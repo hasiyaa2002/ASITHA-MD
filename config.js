5d14efbe0a0885a24406fb0e3f633ce7ecb9d97a
hasiyaa2002 <156837724+hasiyaa2002@users.noreply.github.com>
2024-10-10 16:29:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=uvZQHIzK#7rLdoAqfaiXIeUIAP5V3NdI29pATZEYH642IUPAsCgE",
-MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
+MONGODB: process.env.MONGODB || "mongodb+srv://hasamg:xlwPPyGX6GedR9xD@cluster0.9qiarq4.mongodb.net/",//enter mongo db url
 };
