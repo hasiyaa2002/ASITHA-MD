7a1453660a68765613287ce5b0ee409671740c0e
hasiyaa2002 <156837724+hasiyaa2002@users.noreply.github.com>
2024-10-23 16:24:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=qrhSzJ4T#c7mMAEx3PlFHh_JIc3_PHZKHXuFfwNBs470f0xigsc8",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=WmRxBJzZ#Upkx_MQ3lrcuLCI38rpI5oMTDXsz_f99NYKi4yvrfEE",
 MONGODB: process.env.MONGODB || "mongodb+srv://hasamg:xlwPPyGX6GedR9xD@cluster0.9qiarq4.mongodb.net/",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
