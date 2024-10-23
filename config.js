806fecd09e436e5eb14b2e2b97747ba2f8f349ed
hasiyaa2002 <156837724+hasiyaa2002@users.noreply.github.com>
2024-10-23 20:03:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=WmRxBJzZ#Upkx_MQ3lrcuLCI38rpI5oMTDXsz_f99NYKi4yvrfEE",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=WzpzQRxL#4gHa6fH2R60jDo-6PWmSENv3l8Vb7EP-BaMmDEC7GNs",
 MONGODB: process.env.MONGODB || "mongodb+srv://hasamg:xlwPPyGX6GedR9xD@cluster0.9qiarq4.mongodb.net/",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
